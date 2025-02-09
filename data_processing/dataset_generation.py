254b0c23135cb590029783bd64c434c6b18432ec
Mikhail Kalinichenko <info@neurolumber.ru>
2025-02-09 22:31:10 +0300

    decompose to functions

--- data_processing/dataset_generation.py
+++ data_processing/dataset_generation.py
@@ -1,3 +1,4 @@
+import random
 import time
 from pathlib import Path
 
@@ -14,15 +15,22 @@ from .image_processing import (
 
 
 class DatasetGenerator:
-    def __init__(self):
+    def __init__(self, n_images, relative_object_size_range, object_count_range):
+        self.n_images = n_images
+        s1, s2 = relative_object_size_range
+        self.object_size_range = (int(1 / s1), int(1 / s2))
+        self.object_count_range = object_count_range
         self.bg_transforms = A.Compose(
             [
+                A.AdditiveNoise(),
                 A.HorizontalFlip(p=0.5),
                 A.VerticalFlip(p=0.5),
-                A.RandomBrightnessContrast(p=0.5),
+                A.RandomBrightnessContrast(brightness_limit=(-0.5, 0), p=1.0),
                 A.Rotate(limit=45, p=0.5),
                 A.Blur(blur_limit=(3, 7), p=0.5),
                 A.Defocus(radius=(4, 8), alias_blur=(0.2, 0.4)),
+                A.RandomRain(),
+                A.RandomSnow(),
             ]
         )
         self.object_transforms = A.Compose(
@@ -30,8 +38,56 @@ class DatasetGenerator:
                 A.RandomBrightnessContrast(p=0.5),
                 A.Blur(blur_limit=(3, 7), p=0.5),
                 A.Defocus(radius=(4, 8), alias_blur=(0.2, 0.4)),
+                A.RandomBrightnessContrast(brightness_limit=(0.0, 0.3), p=0.8),
             ]
         )
+
+    def __bbox2yolo(self, bbox, cls_id, img_w, img_h):
+        x, y, w, h = bbox
+        x = x + w / 2
+        y = y + h / 2
+        x, y, w, h = x / img_w, y / img_h, w / img_w, h / img_h
+        return f"{cls_id} {x} {y} {w} {h}"
+
+    def __save_results(self, image, annotation, dst_folder):
+        out_filename = f"img_{int(time.time_ns())}"
+        cv2.imwrite(
+            str(Path(dst_folder, "images", f"{out_filename}.jpg")),
+            image,
+        )
+        with open(Path(dst_folder, "labels", f"{out_filename}.txt"), "w") as f:
+            for i in annotation:
+                f.write(i)
+                f.write("\n")
+        time.sleep(0.1)
+
+    def __create_dst_dirs(self, dst_folder):
+        img_folder = Path(dst_folder, "images")
+        img_folder.mkdir(parents=True, exist_ok=True)
+        label_folder = Path(dst_folder, "labels")
+        label_folder.mkdir(exist_ok=True, parents=True)
+
+    def __generate_object_size(self, bg_width, bg_height):
+        """
+        generate the size of the inserted object relative to the size
+        of the background image
+
+        bg_width - background image width
+
+        bg_width - background image height
+        """
+        min_size = min(bg_height, bg_width)
+        resize_coef = np.random.randint(
+            self.object_size_range[0], self.object_size_range[1]
+        )
+        return min_size // resize_coef, min_size // resize_coef
+
+    def __generate_object_position(self, target_size, object_size):
+        bg_w, bg_h = target_size
+        obj_w, obj_h = object_size
+        x = random.randint(0, bg_w - obj_w)
+        y = random.randint(0, bg_h - obj_h)
+        return x, y
 
     def generate_dataset(
         self,
@@ -48,36 +104,64 @@ class DatasetGenerator:
 
         annotation_file - path to file with annotation in CVAT 1.0 format
         """
+        self.__create_dst_dirs(result_folder)
+
         annotations = parse_cvat_annotations(annotation_file)
         bg_images = list(Path(background_images_folder).glob("*.jpg"))
 
-        for num, bg_img_path in enumerate(bg_images):
-            print("image", bg_img_path)
-            bg_img = cv2.imread(bg_img_path)
-            if bg_img is None:
-                print(f"can not read image {bg_img}")
-                continue
-            bg_h, bg_w, _ = bg_img.shape
-            min_size = min(bg_h, bg_w)
-            bg_img = self.bg_transforms(image=bg_img)["image"]
-            for money in object_names:
-                print("paste", money)
-                ann = annotations[money]
-                money_number = np.random.randint(len(ann))
-                full_money_img = cv2.imread(
-                    str(Path(object_images_folder, ann[money_number]["image_path"]))
+        saved_imgs = 0
+        while saved_imgs < self.n_images:
+            for num, bg_img_path in enumerate(bg_images):
+
+                bg_img = cv2.imread(bg_img_path)
+                if bg_img is None:
+                    print(f"can not read image {bg_img}")
+                    continue
+
+                bg_img = self.bg_transforms(image=bg_img)["image"]
+
+                bg_h, bg_w, _ = bg_img.shape
+
+                objects_markup_list = []
+                n_objects = random.randint(
+                    self.object_count_range[0], self.object_count_range[1]
                 )
-                full_money_img = self.object_transforms(image=full_money_img)["image"]
-                money_img, (money_w, money_h) = cut_polygon_from_image(
-                    full_money_img, ann[money_number]["polygon"]
-                )
-                resize_coef = np.random.randint(5, 8)
-                money_dsize = min_size // resize_coef, min_size // resize_coef
-                resized_money_image = cv2.resize(money_img, money_dsize)
-                rotated = rotate_image(resized_money_image, np.random.randint(0, 360))
-                x = np.random.randint(0, bg_w - money_dsize[0])
-                y = np.random.randint(0, bg_h - money_dsize[1])
-                bg_img = paste_polygon_on_image(bg_img, rotated, (x, y))
-            print("save image", num)
-            cv2.imwrite(str(Path(result_folder, f"img_{int(time.time())}.jpg")), bg_img)
-            time.sleep(1)
+                for _ in range(n_objects):
+                    # for cls_id, money in enumerate(object_names):
+                    cls_id = random.randint(0, len(object_names))
+                    object_name = object_names[cls_id]
+                    ann = annotations[object_name]
+
+                    object_sample_number = np.random.randint(len(ann))
+                    full_money_img = cv2.imread(
+                        str(
+                            Path(
+                                object_images_folder,
+                                ann[object_sample_number]["image_path"],
+                            )
+                        )
+                    )
+                    full_money_img = self.object_transforms(image=full_money_img)[
+                        "image"
+                    ]
+                    object_img, (money_w, money_h) = cut_polygon_from_image(
+                        full_money_img, ann[object_sample_number]["polygon"]
+                    )
+                    object_dsize = self.__generate_object_size(bg_w, bg_h)
+                    resized_money_image = cv2.resize(object_img, object_dsize)
+
+                    rotated = rotate_image(
+                        resized_money_image, np.random.randint(0, 360)
+                    )
+
+                    obj_position = self.__generate_object_position(
+                        (bg_w, bg_h), object_dsize
+                    )
+                    bg_img = paste_polygon_on_image(bg_img, rotated, obj_position)
+
+                    money_bbox = *obj_position, *object_dsize
+                    objects_markup_list.append(
+                        self.__bbox2yolo(money_bbox, cls_id, bg_w, bg_h)
+                    )
+                self.__save_results(bg_img, objects_markup_list, Path(result_folder))
+                saved_imgs += 1
